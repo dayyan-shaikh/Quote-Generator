f499d9c66ed722453e1785ee48ffebc51d53dfa7
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-02-15 11:58:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"
 
 function App() {
+    const [quote,setQuote]=useState("");
+    const [author,setAuthor]=useState("");
+
+    useEffect(()=>{
+      fetch("http://api.quotable.io/random")
+      .then(res=>res.json())
+      .then(
+        (quote)=>{
+          setQuote(quote.content);
+          setAuthor(quote.author);
+        }
+      )
+    },[]);
+
+    let fetchnewquote = ()=>{
+      fetch("http://api.quotable.io/random")
+      .then(res=>res.json())
+      .then(
+        (quote)=>{
+          setQuote(quote.content);
+          setAuthor(quote.author);
+        }
+      )
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-box">
+       <h1>Quote of the Day</h1>
+        <blockquote>{quote}</blockquote>
+        <span>{author}</span>
+        <button onClick={fetchnewquote}>New Quote</button>
     </div>
   );
 }
